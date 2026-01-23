e1981ae68cd980aed41473305c94085a94438b21
at0micBadger <at0mic_badger@yahoo.com>
2026-01-23 18:36:56 -0500

    Add files via upload

---
+++ helloWorld.js
@@ -0,0 +1,6 @@
+
+// hello world output test
+
+console.log(' ');
+console.log('H E L L O  W O R L D!');
+console.log(' ');
---
+++ vars1.js
@@ -0,0 +1,44 @@
+// JAVASCRIPT VARIABLES DEMO 
+
+
+// SOME STRINGS AS NAMES:
+const NAME1 = 'Fred';
+let name2 = 'Dave';
+
+console.log("");
+console.log("PROGRAM START.");
+console.log("");
+console.log("HERE ARE SOME NAMES: ");
+console.log("The names are: " + NAME1 + " and " + name2 + ".");
+console.log("");
+
+// SOME NUMBERS AS VARIABLES
+const PI = 3.14159;
+let num1 = 9.99;
+let num2 = 8;
+
+console.log("");
+console.log("HERE ARE SOME NUMBERS: ");
+console.log("The value of PI is: " + PI + ".");
+console.log("One number available is:" + num2 + ".");
+let num3 = num1 + num2;
+console.log("The numbers combined are: " + num3 + ".");
+console.log("");
+
+
+//SOME BOOLEAN VALUES
+const IS_UNCHANGEABLE = true;
+let cannotChange = false;
+
+console.log("");
+console.log("HERE ARE SOME BOOLEAN VALUES: ");
+console.log("First Value: " + IS_UNCHANGEABLE + ".");
+console.log("Second Value: " + cannotChange + ".");
+console.log("");
+console.log("END OF PROGRAM.");
+console.log("");
+
+
+
+
+
